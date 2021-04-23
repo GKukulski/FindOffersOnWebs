8db0911558d3632005713f151fd9dc39816fcf8e
Grzegorz Kukulski <grzegorz.kukulski@gmail.com>
2021-04-23 12:33:32 +0200

    Spring initializr

---
+++ searchOnPage/src/main/java/myProgram/searchOnPage/SearchOnPageApplication.java
@@ -0,0 +1,13 @@
+package myProgram.searchOnPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchOnPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchOnPageApplication.class, args);
+	}
+
+}
